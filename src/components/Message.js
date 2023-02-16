d0e0c6e21d7db6291bab5086f7ddcfb21324adf7
erinh <erin.hazelwood@outlook.com>
2023-02-16 13:55:22 -0500

    State - completed

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
 class Message extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            message: 'Welcome visitor'
+        }
+    }
+
+    changeMessage() {
+        this.setState({
+            message:'Thank you for subscribing'
+        })
+    }
+
     render() {
         return (
-        <h1>
-            Welcome visitor
-        </h1>
-        )
+        <div>
+            <h1>{this.state.message}</h1>
+            <button onClick={() => this.changeMessage()}>Subscribe</button>
+        </div>
+        ) 
+        
     }
 }
 
