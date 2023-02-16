73c6bf12c5088e2a5be1af933cfcdec0396fe50e
erinh <erin.hazelwood@outlook.com>
2023-02-16 13:36:47 -0500

    State - 3:57

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 class Message extends Component {
     render() {
-        return <h1>Welcome {this.props.name} a.k.a. {this.props.heroName}</h1>
+        return (
+        <h1>
+            Welcome visitor
+        </h1>
+        )
     }
 }
 
