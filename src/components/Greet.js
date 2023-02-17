153dd4d6f9ff45d8801f4a2c7dacd2a575b6bfe3
erinh <erin.hazelwood@outlook.com>
2023-02-17 04:49:07 -0500

    Destructuring props and state - completed

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,14 +4,14 @@ import React from 'react';
   //  return <h1>Hello Erin</h1>
 //}
 
-export const Greet = (props) => {
-  console.log(props)
-  
+export const Greet = props => {
+  const {name, heroName} = props
   return (
     <div>
-  <h1>Hello {props.name} a.k.a. {props.heroName}</h1>
-  {props.children}
-  </div>
+      <h1>
+        Hello {name} a.k.a. {heroName}
+      </h1>
+    </div>
   )
 }
 
