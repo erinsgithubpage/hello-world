e79da70bd7b1f296e9380b3aad907c803723e327
erinh <erin.hazelwood@outlook.com>
2023-02-17 17:43:13 -0500

    19 - 3:33

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Person from './Person';
 
 function NameList() {
-    const names = ['Bruce', 'Clark', 'Diana']
+    const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
     const persons = []
-    const nameList = names.map (name => <h2 key={name}>{name}</h2>)
+    const nameList = names.map ((name, index) => <h2 key={index}>{index} {name}</h2>)
     return <div>{nameList}</div>
    }
 
