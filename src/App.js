3fdaad7c469698cb9faf17bca5e0df92e7e8ad28
erinh <erin.hazelwood@outlook.com>
2023-02-17 05:20:22 -0500

    Event Handling - completed

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FunctionClick />
+        <ClassClick />
         {/*<Counter />*/}
         {/*<Message />*/}
         {/*<Greet name="Bruce" heroName="Batman"><p>This is children props</p></Greet>
         <Greet name="Clark" heroName="Superman">
           <button>Action</button>
         </Greet>*/}
-        <Greet name="Diana" heroName="Wonder Woman"/>
-         <Welcome name="Bruce" heroName="Batman"/>
+        {/*<Greet name="Diana" heroName="Wonder Woman"/>*/}
+         {/*<Welcome name="Bruce" heroName="Batman"/>**}
          {/*<Welcome name="Clark" heroName="Superman"/>
          <Welcome name="Diana" heroName="Wonder Woman"/>*/}
         {/* <Hello />*/}
