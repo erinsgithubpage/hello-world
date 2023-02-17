bf6e47455369e1338a74e43fb4865906d5e8345f
erinh <erin.hazelwood@outlook.com>
2023-02-17 11:07:22 -0500

    14 - 8:51

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -8,6 +8,8 @@ class EventBind extends Component {
       this.state = {
          message: 'Hello'
       }
+
+      this.clickHandler = this.clickHandler.bind(this)
     }
     
 clickHandler() {
@@ -22,7 +24,8 @@ clickHandler() {
       <div>
         <div>{this.state.message}</div>
         {/*<button onClick={this.clickHandler.bind(this)}>Click</button>*/}
-        <button onClick={() => this.clickHandler()}>Click</button>
+        {/*<button onClick={() => this.clickHandler()}>Click</button>*/}
+        <button onClick={this.clickHandler}>Click</button>
       </div>
     )
   }
