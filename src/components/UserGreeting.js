cf168c4702367f4acd680519b35ff920d8fa4529
erinh <erin.hazelwood@outlook.com>
2023-02-17 12:07:59 -0500

    16 - completed

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -12,15 +12,26 @@ class UserGreeting extends Component {
  
  
     render() {
-        if(this.state.isLoggedIn) {
-           return (
-            <div>Welcome Erin</div>
-           ) 
+
+        return this.state.isLoggedIn && <div>Welcome Erin</div>
+
+        /*return (
+            this.state.isLoggedIn ? (<div>Welcome Erin </div>) : (<div>Welcome Guest</div>)
+        )*/
+
+        /*let message 
+        if (this.state.isLoggedIn) {
+            message = <div>Welcome Erin</div>
         } else {
-            return (
-                <div>Welcome Guest</div>  
-            )
+            message = <div>Welcome Guest</div>
         }
+
+        return <div>{message}</div>*/
+       // if(this.state.isLoggedIn) {
+         //  return <>Welcome Erin<///div>
+         //} else {
+         //   return <div>Welcome Guest</div>  
+        //}
        
    /* return (
         <div>
