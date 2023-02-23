307a1c9b2096d55652899ae39194bb9b7c6e6978
erinh <erin.hazelwood@outlook.com>
2023-02-23 11:22:58 -0500

    Video 20 -complete

--- src/App.js
+++ src/App.js
@@ -14,12 +14,18 @@ import UserGreeting from './components/UserGreeting'
 import NameList from './components/NameList'
 //import Person from './components/Person';
 import Stylesheet from './components/Stylesheet'
+import Inline from './components/Inline'
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Stylesheet primary={true} />
+        <h1 className='error'>Error</h1>
+        <h1 className={styles.success}>Success</h1>
+        <Inline />
+       {/*<Stylesheet primary={true} />*/}
         {/*<NameList />*/}
         {/*<UserGreeting />*/}
         {/*<ParentComponent />**}
